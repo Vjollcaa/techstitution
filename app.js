0f7ed50fbf4484ff7dba48c1334e5d666a4a41ca
Vjollcaa <vjollca.kastrati94@gmail.com>
2017-12-27 21:31:22 +0100

    Added some comments

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ MongoClient.connect(mongoURL, function(err, database){
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// use static files
 app.use(express.static(path.join(__dirname + '/public')));
 app.use('/qkmk/', express.static(path.join(__dirname + '/public')));
 app.use('/qkmk/edit/', express.static(path.join(__dirname + '/public')));
@@ -37,7 +38,6 @@ app.use(bodyParser.urlencoded({extended: false}));
  var dateTimeNow = new Date();
 
 // Routs
-
 app.get('/', function (req, res) {
 	 qkmk.find({}).toArray(function(err, docs) {
 		if(err) {
@@ -83,6 +83,7 @@ app.get('/roads/:id', function(req, res) {
     });
 });
 
+// Add data to collection qkmk
 app.post('/qkmk/add', function(req, res) {
 	qkmk.insert({pikaKufitare: req.body.pikaKufitare, 
 		          hyrjeMinMinuta: req.body.hyrjeMinMinuta,
@@ -101,6 +102,7 @@ app.post('/qkmk/add', function(req, res) {
 		          });
 });
 
+//Add data to collection roads
 app.post('/roads/add', function(req, res) {
 	roads.insert({magjistralja: req.body.magjistralja, 
 		          debore: req.body.debore,
@@ -116,6 +118,7 @@ app.post('/roads/add', function(req, res) {
 		          });
 });
 
+//Edit data from collection qkmk
 app.get('/qkmk/edit/:id', function(req, res) {
     qkmk.findOne({_id: ObjectId(req.params.id)}, function(err, doc) {
     	if(err) {
@@ -125,6 +128,7 @@ app.get('/qkmk/edit/:id', function(req, res) {
     });
 });
 
+//Edit data from collection roads
 app.get('/roads/edit/:id', function(req, res) {
     roads.findOne({_id: ObjectId(req.params.id)}, function(err, doc) {
     	if(err) {
@@ -134,6 +138,7 @@ app.get('/roads/edit/:id', function(req, res) {
     });
 });
 
+//Upadate data from collection qkmk
 app.post('/qkmk/update/:id', function(req, res) {
 	qkmk.updateOne({_id: ObjectId(req.params.id)}, 
 		            {$set: {pikaKufitare: req.body.pikaKufitare, 
@@ -153,6 +158,7 @@ app.post('/qkmk/update/:id', function(req, res) {
    });
 });
 
+//Update data from collection roads
 app.post('/roads/update/:id', function(req, res) {
 	roads.updateOne({_id: ObjectId(req.params.id)}, 
 		            {$set: {magjistralja: req.body.magjistralja, 
@@ -169,6 +175,7 @@ app.post('/roads/update/:id', function(req, res) {
    });
 });
 
+//Delete data from collection qkmk
 app.get('/qkmk/delete/:id', function(req, res) {
 		qkmk.deleteOne({_id: ObjectId(req.params.id)}, function(err, result) {
 		if(err) {
@@ -178,6 +185,7 @@ app.get('/qkmk/delete/:id', function(req, res) {
    });
 });
 
+//Delete data from collection roads
 app.get('/roads/delete/:id', function(req, res) {
 		roads.deleteOne({_id: ObjectId(req.params.id)}, function(err, result) {
 		if(err) {
@@ -186,11 +194,6 @@ app.get('/roads/delete/:id', function(req, res) {
 	res.redirect("/roads");
    });
 });
-/*app.get('/roads/', function(req, res) {
-
-	res.redirect("/roads");
-
-}); */
 
 app.listen(3001, function() {
 	console.log("App running at http://localhost:3001");
